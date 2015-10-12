ead8f203bab48875cce91804f0a09b213eb28db6
Cássio Sousa <cassiosantossousa@gmail.com>
2015-10-12 03:02:50 -0300

    Wrote beginning of GameWriter

--- src/writer/GameWriter.java
+++ src/writer/GameWriter.java
@@ -1,12 +1,23 @@
 package writer;
 
+import board.Board;
+import player.AbstractPlayer;
+
 /**
  * @author Cassio dos Santos Sousa
  * @version 1.0
  */
 public class GameWriter {
 
-    private static final String OUTPUT_FOLDER = "../../res/output/";
-    private static final String INPUT_FOLDER = "../../res/input/";
+    private static final String OUTPUT_FOLDER = "res/output/";
+    private static final String INPUT_FOLDER = "res/input/";
+
+    private static final String ENDGAMES = "endgames.txt";
+    private static final String STATS = "stats.txt";
+
+    private String gameOutput(Board endGame, AbstractPlayer p1, AbstractPlayer p2) {
+        String out = "";
+        return out;
+    }
 
 }
